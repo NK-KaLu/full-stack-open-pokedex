ddd7cd882621b8bff2a0c798ba35f28f35c71bd6
Niko Kallio <niko.kallio99@outlook.com>
2024-01-10 14:10:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
@@ -18,9 +18,9 @@ app.get('/version', (req, res) => {
 
 
 app.get('/health', (req, res) => {
-  res.status(500).send('Internal Server Error')
+  throw 'error...'
   // eslint-disable-next-line no-unreachable
-  //res.send('ok')
+  res.send('ok')
 })
 
 
