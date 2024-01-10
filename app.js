6cea03493fe2a96266a31aaa183762d23ffd3d1b
Niko Kallio <niko.kallio99@outlook.com>
2024-01-10 17:59:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/version', (req, res) => {
 
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  //throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
